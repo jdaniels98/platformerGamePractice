5d4739459d645b04c56de0cc73026c0d12cd9060
jdaniels98 <jackdanielswork98@gmail.com>
2022-10-24 21:19:20 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame, sys
+from settings import *
+from tiles import Tile
+
+# Pygame Setup
+pygame.init()
+screen = pygame.display.set_mode((screen_width,screen_height))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit
+            
+    screen.fill("black")
+
+    pygame.display.update()
+    clock.tick(60)
