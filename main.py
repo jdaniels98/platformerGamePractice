344e03a71b8e6b6bd2ba6cb84fee01f962e58175
jdaniels98 <jackdanielswork98@gmail.com>
2022-10-24 21:45:16 +0100

    add tile display to screen

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame, sys
 from settings import *
 from tiles import Tile
+from level import Level
 
 # Pygame Setup
 pygame.init()
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map,screen)
 
 while True:
     for event in pygame.event.get():
@@ -14,6 +16,7 @@ while True:
             sys.exit
             
     screen.fill("black")
+    level.run()
 
     pygame.display.update()
     clock.tick(60)
--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 level_map = [
-'                           ',
-'                           ',
-'                           ',
-'XX    XXX            XX    ',
-'XX                         ',
-'XXXX        XX          XX ',
-'XXXX      XX               ',
-'XX    X  XXXX    XX  XX    ',
-'      X  XXXX    XX  XXX   ',
-'   XXXX  XXXXXX  XX  XXXX  ',
-'XXXXXXX  XXXXXX  XX  XXXX  ']
+'                            ',
+'                            ',
+'                            ',
+' XX    XXX            XX    ',
+' XX                         ',
+' XXXX       XXX          XX ',
+' XXXX      XX               ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+' XXXXXXX  XXXXXX  XX  XXXX  ']
 
 tile_size = 64
 screen_width = 1200
